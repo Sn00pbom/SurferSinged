bb33587fe0d57c725f9a3db633221f2f8fac996e
Sn00pbom <averagegooch@gmail.com>
2016-12-15 12:21:19 -0700

    Pasted Code :^)

--- Program.cs
+++ Program.cs
@@ -3,13 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
 
 namespace SurferSinged
 {
+
+    /*
+     * 
+     * 
+     * 
+     * TODO LIST:
+     * debug and test automated spells, primarily fling
+     * test summoner spells (ESPECIALLY TELE)
+     * TEST AUTOLAUGH AND MASTERY FROM SingedSpell Class
+     * 
+     */
+
     class Program
     {
+
+
+        private static AIHeroClient Me => Player.Instance;
         static void Main(string[] args)
         {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
         }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            //asdf
+            if (!Me.ChampionName.Equals("Singed")) return;
+
+            SingedSpell.LoadSpells();
+
+            Chat.Print("African Singed Loaded!");
+
+
+            Game.OnTick += OnTick;
+
+            Spellbook.OnCastSpell += OnCastSpell;
+
+
+        }
+        private static void OnTick(EventArgs args)
+        {
+
+            if (Me.IsDead || isRecalling()) return;
+
+
+
+        }
+        static Boolean isRecalling()
+        {
+            //ALL BUFFS, POSITIVE, NEGATIVE, OR NEUTRAL COUNT AS BUFFS
+            if (Me.HasBuff("Recall"))
+            {
+                Chat.Print("Recalling...");
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private static void OnCastSpell(Object sender, EventArgs args)
+        {
+            //DEBUG METHOD
+            Chat.Print("Spell Casted!");
+
+        }
+
     }
 }
--- SingedSpell.cs
+++ SingedSpell.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,50 @@ using System.Threading.Tasks;
 
 namespace SurferSinged
 {
+
+
     class SingedSpell
     {
+        // S1 summoner 1 will always be ghost
+        // S2 summoner 2 will always be teleport
+        private static Spell.Active Q, R, S1;
+        private static Spell.SimpleSkillshot W;
+        private static Spell.Targeted E, S2;
+
+        private Boolean poisonOn = false;
+
+        public static void LoadSpells()
+        {
+            Q = new Spell.Active(SpellSlot.Q);
+            R = new Spell.Active(SpellSlot.R);
+            S1 = new Spell.Active(SpellSlot.Summoner1);
+            W = new Spell.SimpleSkillshot(SpellSlot.W);
+            E = new Spell.Targeted(SpellSlot.E, 125); //125 is singed fling range
+            S2 = new Spell.Targeted(SpellSlot.Summoner2, 1000000000); //Teleport: hopefully this range is large enough to be global Kappa
+
+        }
+        public static void TryE()
+        {
+            //MUST BE RUN ON TICK TO GET TARGET
+            var target = TargetSelector.GetTarget(E.Range, DamageType.Magical);
+            if (target != null && E.CanCast(target) == true)
+            {
+                E.Cast();
+            }
+        }
+        public static void ToggleQ()
+        {
+            while (Q.IsOnCooldown)
+            {
+                //INFINITE LOOP to wait for GARENteed Q toggle
+            }
+            Q.Cast();
+
+        }
+        public static void bm()
+        {
+            Player.DoEmote(Emote.Laugh);
+            Player.DoMasteryBadge();
+        }
     }
 }
