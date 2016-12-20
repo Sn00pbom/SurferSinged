1ad2e4a9b4cbd1623a5a9c2bb332e18ea7a51c45
S-F-Horn <samuel.f.horn@gmail.com>
2016-12-19 23:07:21 -0500

    ShopAI comments

--- AI Logic/ShopAI.cs
+++ AI Logic/ShopAI.cs
@@ -8,7 +8,30 @@ using System.Threading.Tasks;
 
 namespace SurferSinged.AI_Logic
 {
-    class ShopAI{
+    /* LIST OF SINGED ITEMS
+     *
+     * ITEM                       ID    DESCRIPTION
+     * Refillable Potion..........2031..Starting item
+     * The Dark Seal..............1082..Starting item
+     * 
+     * Corrupting Potion..........2033..Get on first back/execute at wave 3
+     * Boots of Speed.............1001..Get after refill potion
+     * Boots of Swiftness.........3009..
+     * Mercury's Treads...........3111..Check enemy CC strength? otherwise just get swiftness boots 100%
+     * 
+     * Zz'Rot Portal..............3512..First or Second item
+     * Rylai's Crystal Scepter....3116..First or Second item
+     * 
+     * Dead Man's Plate...........3742..Third item usually
+     * Liandry's Torment..........3151..Third item IF allied team lacks AP
+     * 
+     * Banshee's Veil.............3102..Fourth or fifth item
+     * Thornmail..................3075..Fourth or fifth item 
+     * Frozen Heart...............3110..Fourth or fifth item
+     */
+
+    class ShopAI
+    {
 
         public static AIHeroClient Me = Player.Instance;
         public static AIHeroClient Target;
@@ -17,7 +40,6 @@ namespace SurferSinged.AI_Logic
         {
             gold = Me.Gold;
             return gold;
-            
         }
         public bool hasItem(ItemId item)//returns true/false if user has itemID in their inventory
         {
@@ -31,7 +53,6 @@ namespace SurferSinged.AI_Logic
         }
         public int numItem(ItemId item)//returns number of item in user inv
         {
-            
             TargetSelector.GetTarget(700, DamageType.Physical);
             return 1;
         }
@@ -40,5 +61,10 @@ namespace SurferSinged.AI_Logic
             
         }
         
+    }
+
+    class EvaluateTeams
+    {
+        //Pull 
     }
 }
